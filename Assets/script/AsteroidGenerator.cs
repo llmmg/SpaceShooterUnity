827467a3e6a609142361fa83dfd88dea594a339c
damien.gygi <gygi.damien@gmail.com>
2017-04-29 11:13:49 +0200

    Network asteroid

--- Assets/script/AsteroidGenerator.cs
+++ Assets/script/AsteroidGenerator.cs
@@ -1,19 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class AsteroidGenerator : MonoBehaviour {
+public class AsteroidGenerator : NetworkBehaviour {
     public GameObject AsteroidPrefab;
-    public const int NUMBER_OF_ASTEROIDS = 1000;
+    public const int NUMBER_OF_ASTEROIDS = 500;
     // Use this for initialization
     void Start () {
         //GameObject asteroid = (GameObject)Instantiate(AsteroidPrefab, transform.position, Quaternion.identity);
-        GameObject[] asteroids;
-        asteroids = new GameObject[NUMBER_OF_ASTEROIDS];
+        //GameObject[] asteroids;
+        //asteroids = new GameObject[NUMBER_OF_ASTEROIDS];
         for(int i=0;i<NUMBER_OF_ASTEROIDS; i++)
         {
             GameObject clone = (GameObject)Instantiate(AsteroidPrefab, new Vector3(Random.Range(-250, 250), Random.Range(-250, 250), Random.Range(-250, 250)), Quaternion.Euler(new Vector3(Random.Range(0, 90), Random.Range(0, 90), Random.Range(0, 90))));
-            asteroids[i] = clone;
+            NetworkServer.Spawn(clone);
+            //asteroids[i] = clone;
         }
 
     }
