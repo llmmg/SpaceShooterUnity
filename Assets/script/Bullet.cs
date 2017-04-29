493b92a77c35e0fdecbcc48e5a33969a119208cd
llmmg <lancelotlancelot94@gmail.com>
2017-04-29 10:44:54 +0200

    first commit, only scripts

---
+++ Assets/script/Bullet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Bullet : NetworkBehaviour {
+    public GameObject explosion;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.gameObject.name);
+        GameObject xplosion = (GameObject)Instantiate(explosion, transform.position, Quaternion.identity);
+        
+        Destroy(this.gameObject);
+        Destroy(xplosion,3.0f);
+
+        //print("COLLISION!!!");
+        
+    }
+}
---
+++ Assets/script/Player.cs
@@ -0,0 +1,143 @@
+﻿using System.Timers;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Player : NetworkBehaviour {
+
+    private Rigidbody rb;
+    private Mesh theMesh;
+
+    public float speed;
+    public float horTurnSpeed = 20f;
+    public float verTurnSpeed = 10f;
+
+    public GameObject bulletPrefab;
+
+    public Transform bulletSpawn;
+    public Transform bulletSpawn2;
+
+    //player
+    public Transform myObject;
+    private Vector3[] originalVerts;
+    private Vector3[] rotatedVerts;
+    private float rotateAngleY;
+
+    private float nextActionTime;
+    public float period;
+
+    // Use this for initialization
+    void Start () {
+        rotateAngleY = 0;
+        speed = 10f;
+        /*this.transform.eulerAngles = new Vector3(
+            this.transform.eulerAngles.x,
+            this.transform.eulerAngles.y,
+            this.transform.eulerAngles.z+180
+            );*/
+
+
+        if(!myObject)
+        {
+            myObject = this.transform;
+        }
+        
+        theMesh = myObject.GetComponent<MeshFilter>().mesh as Mesh;
+        originalVerts = new Vector3[theMesh.vertices.Length];
+        originalVerts = theMesh.vertices;
+
+        rotatedVerts = new Vector3[originalVerts.Length];
+
+        //Re-orient object
+        //RotateMesh();
+	}
+    void RotateMesh()
+    {
+        Quaternion qAngle = Quaternion.AngleAxis(rotateAngleY, Vector3.up);
+        for (int vert=0; vert<originalVerts.Length; vert++)
+        {
+            rotatedVerts[vert] = qAngle * originalVerts[vert];
+        }
+
+        theMesh.vertices = rotatedVerts;
+    }
+	
+    void rePos(Rigidbody rb)
+    {
+        if (transform.position.x < 0)
+            rb.position = new Vector3(10, transform.position.y, transform.position.z);
+        if (transform.position.x > 500)
+            rb.position = new Vector3(490, transform.position.y, transform.position.z);
+        if (transform.position.y < 0)
+            rb.position = new Vector3(transform.position.x, 10, transform.position.z);
+        if (transform.position.y > 500)
+            rb.position = new Vector3(transform.position.x, 490, transform.position.z);
+        if (transform.position.z < 0)
+            rb.position = new Vector3(transform.position.x, transform.position.y, 10);
+        if (transform.position.z > 500)
+            rb.position = new Vector3(transform.position.x, transform.position.y, 490);
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate () {
+        //change position of player if off map
+        //rePos(rb);
+        
+        rb.velocity = transform.forward * speed;
+        float x = Input.GetAxis("Vertical");
+        float y = Input.GetAxis("Horizontal");
+
+        
+        rb.AddRelativeTorque(x * verTurnSpeed*Time.deltaTime, y * horTurnSpeed* Time.deltaTime, 0);
+        rb.AddRelativeTorque(y * (-1) * verTurnSpeed * Vector3.forward * Time.deltaTime);
+        //rb.AddRelativeTorque(x * (-1) * verTurnSpeed * Vector3.forward * Time.deltaTime);
+
+
+
+        //shooting 
+        if (Input.GetKey(KeyCode.Space))
+        {          
+            CmdFire();
+        }
+    }
+
+    [Command]
+    void CmdFire()
+    {
+         if(Time.time > nextActionTime)
+        {
+            nextActionTime += period;
+            
+            // Create the Bullet from the Bullet Prefab
+            GameObject bullet = (GameObject)Instantiate(
+                bulletPrefab,
+                bulletSpawn.position,
+                bulletSpawn.rotation);
+
+            GameObject bullet2 = (GameObject)Instantiate(
+                bulletPrefab,
+                bulletSpawn2.position,
+                bulletSpawn2.rotation);
+
+
+            // Add velocity to the bullet
+            bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 60;
+            bullet2.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 60;
+
+            NetworkServer.Spawn(bullet);
+            NetworkServer.Spawn(bullet2);
+
+            // Destroy the bullet after 5 seconds
+            Destroy(bullet, 5.0f);
+            Destroy(bullet2, 5.0f);
+        }
+           
+
+    }
+    
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+}
